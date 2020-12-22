cad5a2e03f3341f586344cfdb2744f38229cd728
sagbeyeg <agbeyegbesisan@gmail.com>
2020-12-22 15:14:33 -0500

    corrected relationships to friednship

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :trips, dependent: :destroy
 
-  has_many :followers, foreign_key: :followed_id , class_name: "Friendship"
+  has_many :followers, foreign_key: :followed_id , class_name: "Friendship", dependent: :destroy
   has_many :followed, through: :followers
-  has_many :followed, foreign_key: :follower_id, class_name: "Friendship"
+  has_many :followed, foreign_key: :follower_id, class_name: "Friendship", dependent: :destroy
   has_many :followers, through: :followed
 
   validates :email, :username ,uniqueness: true 
--- db/migrate/20201222152222_create_friendships.rb
+++ db/migrate/20201222152222_create_friendships.rb
@@ -5,9 +5,9 @@ class CreateFriendships < ActiveRecord::Migration[6.0]
       t.integer :followed_id
       t.timestamps
     end
-    add_index :relationships, :follower_id
-    add_index :relationships, :followed_id
-    add_index :relationships, [:follower_id, :followed_id], unique: true
+    add_index :friendships, :follower_id
+    add_index :friendships, :followed_id
+    add_index :friendships, [:follower_id, :followed_id], unique: true
   end
 end
  
