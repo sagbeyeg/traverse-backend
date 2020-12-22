32103a6b5878eadbaf5214cce59806632a1610cb
sagbeyeg <agbeyegbesisan@gmail.com>
2020-12-22 14:21:44 -0500

    added index for efficiency and to enforce uniqueness

--- db/migrate/20201222152222_create_friendships.rb
+++ db/migrate/20201222152222_create_friendships.rb
@@ -5,6 +5,9 @@ class CreateFriendships < ActiveRecord::Migration[6.0]
       t.integer :followed_id
       t.timestamps
     end
+    add_index :relationships, :follower_id
+    add_index :relationships, :followed_id
+    add_index :relationships, [:follower_id, :followed_id], unique: true
   end
 end
  
