bf5717ebce8926212a79259d71ccb209645594e9
sagbeyeg <agbeyegbesisan@gmail.com>
2020-12-23 10:55:15 -0500

    changed followed to following for clarity & set up follow, unfollow and following? methods in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,23 @@ class User < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :trips, dependent: :destroy
 
-  has_many :followers, foreign_key: :followed_id , class_name: "Friendship", dependent: :destroy
-  has_many :followed, through: :followers
-  has_many :followed, foreign_key: :follower_id, class_name: "Friendship", dependent: :destroy
-  has_many :followers, through: :followed
+  has_many :follower_relationships, foreign_key: :following_id , class_name: "Friendship", dependent: :destroy
+  has_many :followers, through: :follower_relationships
+  has_many :following_relationships, foreign_key: :follower_id, class_name: "Friendship", dependent: :destroy
+  has_many :following, through: :following_relationships
 
   validates :email, :username ,uniqueness: true 
   validates :email, :username, :name, :age, :password, :address, :bio, :city, :state, :country, :zipcode, presence: true
+
+  def follow(user_id)
+    following_relationships.create(following_id: user_id)
+  end
+
+  def unfollow(user_id)
+    following_relationships.find_by(following_id: user_id).destroy
+  end
+
+  def following?(user_id)
+    following.include?(user_id)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_11_184256) do
+ActiveRecord::Schema.define(version: 2020_12_22_152222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "following_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["follower_id", "following_id"], name: "index_friendships_on_follower_id_and_following_id", unique: true
+    t.index ["follower_id"], name: "index_friendships_on_follower_id"
+    t.index ["following_id"], name: "index_friendships_on_following_id"
+  end
 
   create_table "locations", force: :cascade do |t|
     t.string "name"
